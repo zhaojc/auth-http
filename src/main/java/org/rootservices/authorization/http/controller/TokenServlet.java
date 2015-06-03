9ade254d6a040b4f70c76e32c1ab3f2f6a3f8f78
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2015-06-03 08:06:08 -0500

    pt-95240294: loads spring when web app starts.

---
+++ src/main/java/org/rootservices/authorization/http/controller/TokenServlet.java
@@ -0,0 +1,11 @@
+package org.rootservices.authorization.http.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by tommackenzie on 6/3/15.
+ */
+@WebServlet(value="/token", name="tokenServlet")
+public class TokenServlet extends HttpServlet {
+}
